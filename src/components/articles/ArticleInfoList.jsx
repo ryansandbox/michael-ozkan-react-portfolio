4aac41c7d7585217a74ee1229a2c9bed5b5f9725
Ryan Balieiro <ryan.balieiro@gmail.com>
2025-11-24 18:37:40 -0300

    Fix: Add spacing between copy-to-clipboard button and link in order to prevent misclicks.

--- src/components/articles/ArticleInfoList.jsx
+++ src/components/articles/ArticleInfoList.jsx
@@ -121,7 +121,7 @@ function ArticleInfoListItem({ itemWrapper}) {
 
                     {itemWrapper.copyToClipboardButton && (
                         <CopyButton text={itemWrapper.locales.label || itemWrapper.label || itemWrapper.locales.text}
-                                    buttonClassName={`ms-2`}/>
+                                    buttonClassName={`ms-2 ps-1`}/>
                     )}
                 </div>
             </div>
